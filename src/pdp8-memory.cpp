49c0d57cafdae25bd5423ff25473ca4146958d92
esayers <esayers@pdx.edu>
2015-02-03 18:58:30 -0800

    Renamed memory files

---
+++ src/pdp8-memory.cpp
@@ -0,0 +1,18 @@
+/*  pdp8-main-memory.cpp
+ *
+ *  Edward Sayers
+ * ECE 486 Project
+ */
+
+
+
+// Constructor
+ Pdp8MainMemory::Pdp8MainMemory()
+ {
+
+ }
+
+ // Destructor
+ Pdp8MainMemory::~Pdp8MainMemory()
+ {
+ }
--- src/pdp8-memory.h
+++ src/pdp8-memory.h
@@ -10,7 +10,12 @@
 class Pdp8MainMemory
 {
 public:
+    Pdp8MainMemory();
+    ~Pdp8MainMemory();
 private:
+    const unsigned int wordSize = 12;
+    const unsigned int memSize = 4096;
+    unsigned int mem[memSize];
 }
 
 #endif // PDP8_MAIN_MEMORY_H
